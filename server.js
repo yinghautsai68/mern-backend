715f98c7a30ebbce50f6cd09f89ae7af5188705a
yinghau68 <yinghautsai68cycu@gmail.com>
2026-01-06 20:57:54 +0800

    d
    d

--- server.js
+++ server.js
@@ -29,13 +29,10 @@ const server = http.createServer(app)
 
 //MIDDLEWARE
 // Handle preflight requests for all routes
-app.options("*", cors({
+app.use(cors({
     origin: "https://yinghautsai68.github.io",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
 }));
-
 app.use(express.json())
 app.use('/uploads', express.static(join(__dirname, 'uploads')));
 
